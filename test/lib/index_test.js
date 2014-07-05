7952a11650655cb817ba88b47d22cc9e713182dc
Hector Virgen <hvirgen@tagged.com>
2014-07-05 08:02:52 -0700

    Whitespace

--- test/lib/index_test.js
+++ test/lib/index_test.js
@@ -2,17 +2,19 @@ var proximous = require(LIB_DIR);
 
 describe('Proximous', function() {
 
-    beforeEach(function(){
+    beforeEach(function() {
         this.proxy = proximous('http://foo.bar');
     });
 
     it('is a function', function() {
         proximous.should.be.a('function');
     });
-    it('returns a proxy instance', function(){
+
+    it('returns a proxy instance', function() {
         this.proxy.should.be.a('object');
     });
-    it('contains a url argument', function(){
+
+    it('contains a url argument', function() {
 
         this.proxy.should.have.property('url', 'http://foo.bar');
     });
--- test/lib/proxy_test.js
+++ test/lib/proxy_test.js
@@ -5,7 +5,7 @@ describe('Proxy', function() {
         this.proxy = new Proxy('http://foo.bar');
     });
 
-    describe('matchGet', function(){
+    describe('matchGet', function() {
         it('should be a function', function() {
             this.proxy.matchGet.should.be.a('function');
         });
@@ -16,14 +16,17 @@ describe('Proxy', function() {
             this.proxy.matchGet('/index.html');
             this.proxy.isMatch('get', '/index.html').should.be.true;
         });
+
         it('doesn\'t match get with exact string match', function() {
             this.proxy.matchGet('/home.html');
             this.proxy.isMatch('get', '/index.html').should.be.false;
         });       
+
         it('returns false if type doesn\'t match', function() {
             this.proxy.matchGet('/index.html');
             this.proxy.isMatch('post', '/index.html').should.be.false;
         });
+
         it('returns true if type and url matches any of the matchers', function() {
             this.proxy.matchGet('/index.html');
             this.proxy.matchGet('/index1.html');
@@ -40,6 +43,11 @@ describe('Proxy', function() {
             this.proxy.matchAll('/index.html');
             this.proxy.isMatch('post', '/index.html').should.be.true;
             this.proxy.isMatch('get', '/index.html').should.be.true;
+        });
+
+        it('matches get with regex', function() {
+            this.proxy.matchGet(/bar/);
+            this.proxy.isMatch('get', '/foo/bar.html').should.be.true;
         });
 
         it('matches get with regex', function() {
